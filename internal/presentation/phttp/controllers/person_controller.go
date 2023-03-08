049d3478dca7927e4dfcf4cb9582830d5bf98efb
Bruno Alliguieri <devalliguieri@gmail.com>
2023-03-07 22:33:51 -0300

    ref: handler functions extracted

--- internal/presentation/phttp/controllers/person_controller.go
+++ internal/presentation/phttp/controllers/person_controller.go
@@ -23,33 +23,37 @@ func NewPersonController(personService service.PersonService) personController {
 func (pc personController) GetHandlers() map[string]phttp.RouteDefinition {
 	return map[string]phttp.RouteDefinition{
 		"/person": {
-			Method: phttp.POST,
-			HandlingFunc: func(w http.ResponseWriter, r *http.Request) {
-				log.Println("Creating new person...")
-
-				person, err := phttp.ParseBody[models.Person](r.Body)
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusBadRequest)
-					return
-				}
-
-				createdPerson := pc.personService.CreatePerson(*person)
-				phttp.WriteJsonResponse(w, createdPerson)
-			},
+			Method:       phttp.POST,
+			HandlingFunc: pc.createPerson,
 		},
 		"/person/{personId}": {
-			Method: phttp.GET,
-			HandlingFunc: func(w http.ResponseWriter, r *http.Request) {
-				personId, err := strconv.Atoi(phttp.GetUrlParam(r, "personId"))
-
-				if err != nil {
-					http.Error(w, http.StatusText(http.StatusUnprocessableEntity), http.StatusUnprocessableEntity)
-					return
-				}
-
-				person := pc.personService.GetPersonById(personId)
-				phttp.WriteJsonResponse(w, person)
-			},
+			Method:       phttp.GET,
+			HandlingFunc: pc.getPersonById,
 		},
 	}
+}
+
+func (pc personController) createPerson(w http.ResponseWriter, r *http.Request) {
+	log.Println("Creating new person...")
+
+	person, err := phttp.ParseBody[models.Person](r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	createdPerson := pc.personService.CreatePerson(*person)
+	phttp.WriteJsonResponse(w, createdPerson)
+}
+
+func (pc personController) getPersonById(w http.ResponseWriter, r *http.Request) {
+	personId, err := strconv.Atoi(phttp.GetUrlParam(r, "personId"))
+
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusUnprocessableEntity), http.StatusUnprocessableEntity)
+		return
+	}
+
+	person := pc.personService.GetPersonById(personId)
+	phttp.WriteJsonResponse(w, person)
 }
